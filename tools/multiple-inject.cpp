389dd59aa47efadd2ea652685b1296501567d6a8
onioin <canyongudaitis@gmail.com>
2025-03-05 16:08:19 -0800

    added command line parsing

--- tools/multiple-inject.cpp
+++ tools/multiple-inject.cpp
@@ -4,6 +4,7 @@
 
 #include "CFGParser.h"
 #include <stdlib.h>
+#include <string>
 
 /*
  * get config file off command line
@@ -21,11 +22,41 @@
 
 using namespace llvm;
 
-cl::list<CFG_t> ConfigFiles("c");
+cl::OptionCategory optCat{"Multiple Cache Injector Options"};
 
-cl::opt<CFG_t> ConfigFile("C")
+cl::opt<std::string> InPath(cl::Positional, cl::desc("<Module to instrument>"),
+                            cl::value_desc("bitcode filename"),
+                            cl::init(""), cl::Required,
+                            cl::cat(optCat));
+
+cl::opt<std::string> OutFile("o", cl::desc("Output filename"),
+                             cl::value_desc("filename"), cl::Required,
+                             cl::init(""), cl::cat(optCat));
+
+cl::opt<char> OptLevel("O", cl::desc("Clang optimization level [-O0, -O1, -O2(default), -O3]"),
+                       cl::init('2'), cl::Prefix,
+                       cl::value_desc("level"),
+                       cl::cat(optCat));
+
+cl::list<std::string> LibPaths("L", cl::Prefix,
+                               cl::desc("Specify library search paths"),
+                               cl::value_desc("directory"), cl::cat(optCat));
+
+cl::list<std::string> LibNames("l", cl::Prefix,
+                               cl::desc("Specify libraries to link against"),
+                               cl::value_desc("library"), cl::cat(optCat));
+
+cl::list<CFG_t> ConfigFiles("c",
+                            cl::cat(optCat));
+
+cl::opt<CFG_t> ConfigFile("C",
+                          cl::cat(optCat));
 
 int main(int argc, char** argv){
-    cl::ParseCommandLineOptions(argc, argv);
+    cl::HideUnrelatedOptions(optCat);
+    cl::ParseCommandLineOptions(argc, argv, "Multiple Cache Simulator\n\n"
+                                            "   This program allows the user to inject several different caches into "
+                                            "an llvm bitcode file\n"
+                                            "(equivalent to using bin/inject-cache-sim multiple times)");
     exit(EXIT_SUCCESS);
 }
