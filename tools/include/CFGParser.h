92e5822da3e8174cb7c9a79f3bcddbd45983964c
onioin <canyongudaitis@gmail.com>
2025-03-05 16:11:21 -0800

    minor refactor

--- tools/include/CFGParser.h
+++ tools/include/CFGParser.h
@@ -14,42 +14,46 @@
 #include <iostream>
 
 
-typedef struct CFG {
+typedef struct CCFG {
     uint8_t s;
     uint8_t E;
     uint8_t b;
     uint8_t pol;
     char* name;
-} CFG_t;
+} CCFG_t;
 
-template <> class llvm::cl::parser<CFG_t> : public basic_parser<CFG_t>{
+template <> class llvm::cl::parser<CCFG_t> : public basic_parser<CCFG_t>{
 public:
     parser(llvm::cl::Option &O) : basic_parser(O) {}
 
     bool parse(llvm::cl::Option &O, llvm::StringRef ArgName,
-               llvm::StringRef Arg, CFG_t &V);
+               llvm::StringRef Arg, CCFG_t &V);
 
     llvm::StringRef getValueName() const override {return "Cache Config";}
 
-    void printOptionDiff(const llvm::cl::Option &O, CFG_t V,
-                         llvm::cl::OptionValue<CFG_t> Default, size_t GlobalWidth) const;
+    void printOptionDiff(const llvm::cl::Option &O, CCFG_t V,
+                         llvm::cl::OptionValue<CCFG_t> Default, size_t GlobalWidth) const;
 
     void anchor() override {};
 
 };
 
-bool llvm::cl::parser<CFG_t>::parse(llvm::cl::Option &O, llvm::StringRef ArgName,
-                                    llvm::StringRef Arg, CFG_t &V){
-    std::cout << ArgName.str() << std::endl;
+bool llvm::cl::parser<CCFG_t>::parse(llvm::cl::Option &O, llvm::StringRef ArgName,
+                                    llvm::StringRef Arg, CCFG_t &V){
+    std::cout << ArgName.str() << ": " << Arg.str() << std::endl;
     return false;
 }
 
-bool parseCFGList()
+bool parseCCFGList(){
+    return false;
+}
 
-bool parseCFG()
+bool parseCCFG(){
+    return false;
+}
 
-void llvm::cl::parser<CFG_t>::printOptionDiff(const llvm::cl::Option &O, CFG_t V,
-                                              llvm::cl::OptionValue<CFG_t> D,
+void llvm::cl::parser<CCFG_t>::printOptionDiff(const llvm::cl::Option &O, CCFG_t V,
+                                              llvm::cl::OptionValue<CCFG_t> D,
                                               size_t GlobalWidth) const {
     printOptionName(O, GlobalWidth);
     std::string Str("hello world");
